f0ee8289c3d89a5da033d61cdfdbe3447dc4a281
Javi <javichuvj8@gmail.com>
2022-10-05 09:56:04 +0200

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "Javi", password: "password")
+User.create(username: "Carlos", password: "password")
+User.create(username: "Oscar", password: "password")
+User.create(username: "Esteban", password: "password")
+User.create(username: "Isaias", password: "password")
+
+Message.create(body: "New message", user: User.last)
+Message.create(body: "Random text", user: User.first)
+Message.create(body: "Message", user: User.last)
+Message.create(body: "Test message", user: User.first)
